464d57428313bc1a9b8884ef05742d21a38962de
Kübra Yeşilkaya <93487264+kubrayesilkaya@users.noreply.github.com>
2023-08-08 23:22:28 +0300

    Update OrderDBContext.cs

--- Order_OrderDetail/DATA/OrderDBContext.cs
+++ Order_OrderDetail/DATA/OrderDBContext.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Order_OrderDetail.Entities;
 
 namespace Order_OrderDetail.DATA
@@ -7,7 +7,16 @@ namespace Order_OrderDetail.DATA
     {
         public OrderDBContext(DbContextOptions<OrderDBContext> options) : base(options) 
         { }
-        public DbSet<OrderEntity> Order  { get; set; }
+        public DbSet<OrderEntity> Order { get; set; }
         public DbSet<OrderDetailEntity> OrderDetails { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<OrderDetailEntity>()
+                .HasOne(o => o.Order)
+                .WithMany(od => od.OrderDetails)
+                .HasForeignKey(f => f.ID_ORDER);
+                
+        }
     }
 }
