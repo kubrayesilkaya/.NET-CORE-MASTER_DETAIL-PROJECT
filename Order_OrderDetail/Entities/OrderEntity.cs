dc3db30db3d4f023552e22ac1986c157010622a6
Kübra Yeşilkaya <93487264+kubrayesilkaya@users.noreply.github.com>
2023-08-08 23:21:26 +0300

    Update OrderEntity.cs

--- Order_OrderDetail/Entities/OrderEntity.cs
+++ Order_OrderDetail/Entities/OrderEntity.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.VisualBasic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
@@ -14,5 +14,14 @@ namespace Order_OrderDetail.Entities
         public DateTime ORDER_DATE { get; set; }
         public int ITEM_COUNT { get;set; }
         public decimal ORDER_PRICE { get; set; }
+        public ICollection<OrderDetailEntity> OrderDetails { get; set; } //principal (parent)
+
+        //Bir Order'ın birden fazla orderDetail'i olabilir. Çoğul olduğu için OderDetails.
+
+        //Order'ların birden fazla orderDetail'leri olabileceğinden dolayı, bu Order'a karşılık bir koleksiyon tanımladım,
+        //ve bu koleksiyonun ismini de çoğul verdim. Çünkü bu Order'a ait tüm orderDetail'leri öğrenmek istiyor ise, 
+        //bunu orderDetails proporty'sinden elde edeceğim.
+
+        //orderDetails tekil olarak Order'a, Order ise çoğul olarak orderDetails'e bağlı. Bire-çok ilişki.
     }
 }
