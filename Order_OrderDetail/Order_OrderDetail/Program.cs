569e650acd62b02f7f1caa2afe0df93cc88e7985
Kübra Yeşilkaya <kubrayesilkaya.ky@gmail.com>
2023-08-08 23:07:51 +0300

    one to many relationship properties are added.

---
+++ Order_OrderDetail/Order_OrderDetail/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.EntityFrameworkCore;
+using Order_OrderDetail.DATA;
+using Order_OrderDetail.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddTransient<OrderService, OrderService>();
+
+builder.Services.AddDbContext<OrderDBContext>(
+    options => options.UseSqlServer(
+        builder.Configuration.GetConnectionString("connectionString")
+        )
+    );
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
