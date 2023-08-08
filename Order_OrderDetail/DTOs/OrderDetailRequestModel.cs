e11df8a087fed103350ac4d7cf70115d293eafb8
Kübra Yeşilkaya <kubrayesilkaya.ky@gmail.com>
2023-08-08 22:59:31 +0300

    .NET Core Master_Detail Project | One-to-many relationship.

---
+++ Order_OrderDetail/DTOs/OrderDetailRequestModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Order_OrderDetail.DTOs
+{
+    public class OrderDetailRequestModel
+    {
+        public int ORDER_ID_DETAIL { get; set; }
+        public string ITEM_NAME { get; set; }
+        public int ITEM_QUANTITY { get; set; }
+        public string ITEM_UNIT { get; set; }
+        public int ID_ORDER { get; set; }
+    }
+}
---
+++ Order_OrderDetail/Order_OrderDetail/DATA/OrderDBContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Order_OrderDetail.Entities;
+
+namespace Order_OrderDetail.DATA
+{
+    public class OrderDBContext : DbContext
+    {
+        public OrderDBContext(DbContextOptions<OrderDBContext> options) : base(options) 
+        { }
+        public DbSet<OrderEntity> Order { get; set; }
+        public DbSet<OrderDetailEntity> OrderDetails { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<OrderDetailEntity>()
+                .HasOne(o => o.Order)
+                .WithMany(od => od.OrderDetails)
+                .HasForeignKey(f => f.ID_ORDER);
+                
+        }
+    }
+}
